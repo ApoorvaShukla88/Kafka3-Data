a6642842b4fb60a2663dd9fd0e6de633c88f2165
Apoorvashukla88 <apoorvashukla88@gmail.com>
2020-04-20 22:52:41 -0400

    phase1

--- phase1/consumer-to-SQL.py
+++ phase1/consumer-to-SQL.py
@@ -1,12 +1,14 @@
 from kafka import KafkaConsumer, TopicPartition
 from json import loads
+import sqlalchemy
+
 
 class XactionConsumer:
     def __init__(self):
         self.consumer = KafkaConsumer('bank-customer-events',
-            bootstrap_servers=['localhost:9092'],
-            # auto_offset_reset='earliest',
-            value_deserializer=lambda m: loads(m.decode('ascii')))
+                                      bootstrap_servers=['localhost:9092'],
+                                      # auto_offset_reset='earliest',
+                                      value_deserializer=lambda m: loads(m.decode('ascii')))
         ## These are two python dictionarys
         # Ledger is the one where all the transaction get posted
         self.ledger = {}
@@ -16,8 +18,9 @@ class XactionConsumer:
         # THE PROBLEM is every time we re-run the Consumer, ALL our customer
         # data gets lost!
         # add a way to connect to your database here.
+        self.engine = sqlalchemy.create_engine('mysql+pymysql://root:zipcoder@localhost/consumerkafka')
 
-        #Go back to the readme.
+        # Go back to the readme.
 
     def handleMessages(self):
         for message in self.consumer:
@@ -32,7 +35,30 @@ class XactionConsumer:
             else:
                 self.custBalances[message['custid']] -= message['amt']
             print(self.custBalances)
+            print()
+            with self.engine.connect() as con:
+                with con.begin():
+                    con.execute("INSERT INTO consumerZipbank (custid, type, amt) VALUES (int(message['custid']),"
+                                " str(message['type']), int(message['amt'])")
+
+
+
+
+
+
+# class Transaction(Base):
+#     __tablename__ = 'transaction'
+#     # Here we define columns for the table person
+#     # Notice that each column is also a normal Python instance attribute.
+#     id = Column(Integer, primary_key=True)
+#     custid = Column(Integer)
+#     type = Column(String(250), nullable=False)
+#     date = Column(Integer)
+#     amt = Column(Integer)
+
 
 if __name__ == "__main__":
     c = XactionConsumer()
     c.handleMessages()
+   
+
